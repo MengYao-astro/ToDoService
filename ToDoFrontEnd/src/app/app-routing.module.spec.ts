07787ec362b54d925216634281b5d4a8b39c07ba
xyan8 <xyan8@slb.com>
2022-11-07 14:37:46 +0800

    feat: add todos routers and finish app-routing tests

--- ToDoFrontEnd/src/app/app-routing.module.spec.ts
+++ ToDoFrontEnd/src/app/app-routing.module.spec.ts
@@ -2,6 +2,7 @@ import { Location } from '@angular/common';
 import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
+import { routes } from './app-routing.module';
 
 describe('Router', () => {
   let location: Location;
@@ -9,7 +10,7 @@ describe('Router', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
+      imports: [RouterTestingModule.withRoutes(routes)],
       declarations: []
     });
 
@@ -21,5 +22,40 @@ describe('Router', () => {
 
   it('test navigate', fakeAsync(() =>{
 
+  }));
+
+  it('should got todos when navigate ""', fakeAsync(() => {
+    // given 
+    // when
+    router.navigate(['']);
+    tick(50);
+    // then
+    expect(location.path()).toEqual('/todos');
+  }));
+  it('should got todos when navigate "todos/create"', fakeAsync(() => {
+    // given 
+    // when
+    router.navigate(['/todos/create']);
+    tick(50);
+    // then
+    expect(location.path()).toEqual('/todos/create');
+  }));
+
+  it('should got todos item when navigate "/todos/:id"', fakeAsync(() => {
+    // given 
+    // when
+    router.navigate(['/todos/1']);
+    tick(50);
+    // then
+    expect(location.path()).toEqual('/todos/1');
+  }));
+
+  it('should got todos item when navigate "/todos/modify/:id"', fakeAsync(() => {
+    // given 
+    // when
+    router.navigate(['/todos/modify/1']);
+    tick(50);
+    // then
+    expect(location.path()).toEqual('/todos/modify/1');
   }));
 });
